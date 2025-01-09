674fd1fd90429a0f4069a795e7edf9e553547471
WordPress Static Deploy <wordpress-deploy@example.com>
2025-01-09 03:25:44 +0000

    Auto-deploy: Updates including assets and subdirectories - 2025-01-09 03:25:44

---
+++ html/wp-login.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en-US">
+	<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<title>Lost Password ‹ tantawy — WordPress</title>
+	<meta name="robots" content="max-image-preview:large, noindex, noarchive">
+<link rel="stylesheet" href="../css/load-styles.php" media="all">
+	<meta name="referrer" content="strict-origin-when-cross-origin">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		</head>
+	<body class="login no-js login-action-lostpassword wp-core-ui  locale-en-us">
+	<script>
+document.body.className = document.body.className.replace('no-js','js');
+</script>
+
+				<h1 class="screen-reader-text">Lost Password</h1>
+			<div id="login">
+		<h1 role="presentation" class="wp-login-logo"><a href="https://wordpress.org/">Powered by WordPress</a></h1>
+	<div class="notice notice-info message"><p>Please enter your username or email address. You will receive an email message with instructions on how to reset your password.</p></div>
+
+		<form name="lostpasswordform" id="lostpasswordform" action="../html/wp-login.php" method="post">
+			<p>
+				<label for="user_login">Username or Email Address</label>
+				<input type="text" name="user_login" id="user_login" class="input" value="" size="20" autocapitalize="off" autocomplete="username" required="required">
+			</p>
+						<input type="hidden" name="redirect_to" value="">
+			<p class="submit">
+				<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Get New Password">
+			</p>
+		</form>
+
+		<p id="nav">
+			<a class="wp-login-log-in" href="../html/wp-login.php">Log in</a>
+					</p>
+				<p id="backtoblog">
+			<a href="../html/mohamed.html">← Go to tantawy</a>		</p>
+			</div>
+		
+	<script>
+		try{document.getElementById('user_login').focus();}catch(e){}
+		if(typeof wpOnload==='function')wpOnload();
+		
+</script>
+	</body>
+	</html>
